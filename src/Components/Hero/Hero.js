64ad3fc29538edc8d1c13927b413f401dcfd5230
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-05-27 17:19:10 +0200

    prelimanery hero carousel

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,48 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import HeroBackground1 from 'Assets/images/zaak1.jpg';
-import HeroBackground2 from 'Assets/images/zaak2.jpg';
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import './Hero.css';
+
+import Hero1 from 'Assets/images/hero1.jpg';
+import Hero2 from 'Assets/images/hero2.jpg';
+import Hero3 from 'Assets/images/hero3.jpg';
+import Hero4 from 'Assets/images/hero4.jpg';
+
+const changeBg = keyframes`
+    0% {
+        background-image: url(${Hero1});
+    }
+    25% {
+        background-image: url(${Hero2});
+    }
+    50% {
+        background-image: url(${Hero3});
+    }
+    75% {
+        background-image: url(${Hero4});
+    }
+    100% {
+        background-image: url(${Hero1});
+    }
+`;
+
+const StyledHero = styled.section`
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    animation: ${changeBg} 10s infinite;
+`;
 
 const Hero = () => {
-    const [activeBackground, setActiveBackground] = useState(HeroBackground1);
-    const [activeCount, setActiveCount] = useState(0);
-    const backgrounds = [HeroBackground1, HeroBackground2];
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            if (activeCount === backgrounds.length) {
-                setActiveCount(0);
-            } else {
-                setActiveCount(activeCount + 1);
-            }
-
-            console.log(activeCount);
-
-            setActiveBackground(backgrounds[activeCount + 1]);
-            // setActiveCount(activeCount + 1);
-        }, 1000);
-        return () => clearInterval(interval);
-    }, [activeCount, backgrounds]);
-
-    const StyledHero = styled.section`
-        background-image: url(${activeBackground});
-        background-position: center center;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-        background-size: cover;
-        background-color: #999;
-    `;
-
     return (
-        <StyledHero className="hero is-fullheight-with-navbar is-primary">
-            <div className="hero-body">
-                <div className="container has-text-centered">
-                    <h1 className="title is-1 has-text-white">
-                        Sluijter Kappers
-                    </h1>
+        <>
+            <StyledHero className="hero is-fullheight-with-navbar is-primary">
+                <div className="hero-body">
+                    <div className="container has-text-centered">
+                        <h1 className="title is-1 has-text-white">
+                            Sluijter Kappers
+                        </h1>
+                    </div>
                 </div>
-            </div>
-        </StyledHero>
+            </StyledHero>
+        </>
     );
 };
 
