65fbc3a5efd6190ac2cf17c1b358bc2bae2d2c4e
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-06-05 15:47:14 +0200

    updated hero animation

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,49 +1,57 @@
-import React from 'react';
-import styled, { keyframes } from 'styled-components';
+import React, { useEffect } from 'react';
+import 'bulma-carousel/dist/css/bulma-carousel.min.css';
+import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js';
+
 import Hero1 from 'Assets/images/hero1.jpg';
 import Hero2 from 'Assets/images/hero2.jpg';
 import Hero3 from 'Assets/images/hero3.jpg';
 import Hero4 from 'Assets/images/hero4.jpg';
 
-const changeBg = keyframes`
-    0% {
-        background-image: url(${Hero1});
-    }
-    25% {
-        background-image: url(${Hero2});
-    }
-    50% {
-        background-image: url(${Hero3});
-    }
-    75% {
-        background-image: url(${Hero4});
-    }
-    100% {
-        background-image: url(${Hero1});
-    }
-`;
-
-const StyledHero = styled.section`
-    background-position: center center;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    background-size: cover;
-    animation: ${changeBg} 10s infinite;
-`;
-
 const Hero = () => {
+    useEffect(() => {
+        const carousel = bulmaCarousel.attach('#demo', {
+            autoplay: true,
+            autoplaySpeed: 3000,
+            slidesToScroll: 1,
+            slidesToShow: 1,
+            navigation: false,
+        });
+
+        const delay = (a) => new Promise((t) => setTimeout(t, a));
+        const slideReset = async () => {
+            await delay(12000);
+            carousel[0].reset();
+
+            slideReset();
+        };
+
+        slideReset();
+    }, []);
+
     return (
-        <>
-            <StyledHero className="hero is-fullheight-with-navbar is-primary">
-                <div className="hero-body">
-                    <div className="container has-text-centered">
-                        <h1 className="title is-1 has-text-white">
-                            Sluijter Kappers
-                        </h1>
-                    </div>
+        <section className="hero is-fullheight-with-navbar is-primary has-carousel">
+            <div id="demo" className="hero-carousel">
+                <div className="has-background is-active">
+                    <img className="is-background" src={Hero1} alt="" />
                 </div>
-            </StyledHero>
-        </>
+                <div className="has-background">
+                    <img className="is-background" src={Hero2} alt="" />
+                </div>
+                <div className="has-background">
+                    <img className="is-background" src={Hero3} alt="" />
+                </div>
+                <div className="has-background">
+                    <img className="is-background" src={Hero4} alt="" />
+                </div>
+            </div>
+            <div className="hero-body">
+                <div className="container has-text-centered">
+                    <h1 className="title is-1 has-text-white">
+                        Sluijter Kappers
+                    </h1>
+                </div>
+            </div>
+        </section>
     );
 };
 
