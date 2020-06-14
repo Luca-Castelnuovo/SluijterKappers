8918a1190e146f4882013a2c0dfe895ff45f0178
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-06-14 15:14:35 +0200

    update hero slider

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import styled from 'styled-components';
 import 'bulma-carousel/dist/css/bulma-carousel.min.css';
 import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js';
 
@@ -7,14 +8,20 @@ import Hero2 from 'Assets/images/hero2.jpg';
 import Hero3 from 'Assets/images/hero3.jpg';
 import Hero4 from 'Assets/images/hero4.jpg';
 
+const Slide = styled.div`
+    height: 100%;
+`;
+
 const Hero = () => {
     useEffect(() => {
-        const carousel = bulmaCarousel.attach('#demo', {
+        const carousel = bulmaCarousel.attach('.hero-carousel', {
             autoplay: true,
             autoplaySpeed: 3000,
             slidesToScroll: 1,
             slidesToShow: 1,
+            pagination: false,
             navigation: false,
+            effect: 'fade',
         });
 
         const delay = (a) => new Promise((t) => setTimeout(t, a));
@@ -29,27 +36,23 @@ const Hero = () => {
     }, []);
 
     return (
-        <section className="hero is-fullheight-with-navbar is-primary has-carousel">
-            <div id="demo" className="hero-carousel">
-                <div className="has-background is-active">
+        <section className="hero is-fullheight-with-navbar has-carousel is-primary">
+            <div className="hero-carousel">
+                <Slide className="has-background is-active">
                     <img className="is-background" src={Hero1} alt="" />
-                </div>
-                <div className="has-background">
+                </Slide>
+                <Slide className="has-background">
                     <img className="is-background" src={Hero2} alt="" />
-                </div>
-                <div className="has-background">
+                </Slide>
+                <Slide className="has-background">
                     <img className="is-background" src={Hero3} alt="" />
-                </div>
-                <div className="has-background">
+                </Slide>
+                <Slide className="has-background">
                     <img className="is-background" src={Hero4} alt="" />
-                </div>
+                </Slide>
             </div>
-            <div className="hero-body">
-                <div className="container has-text-centered">
-                    <h1 className="title is-1 has-text-white">
-                        Sluijter Kappers
-                    </h1>
-                </div>
+            <div className="hero-body has-text-centered">
+                <h1 className="title is-1 has-text-white">Sluijter Kappers</h1>
             </div>
         </section>
     );
