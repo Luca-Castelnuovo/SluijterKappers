9d55afe89c8ffd58fda32dc96785b473f889fe1c
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-07-20 13:49:37 +0200

    removed insta icon

--- src/Components/NavBar/Brand/BrandHamburger.js
+++ src/Components/NavBar/Brand/BrandHamburger.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-
-import MenuLink from '../Menu/MenuLink';
 
 const BrandHamburger = ({ hamburgerOpen, hamburgerClick }) => {
     const classes = classNames('navbar-burger', 'burger', {
@@ -12,21 +8,11 @@ const BrandHamburger = ({ hamburgerOpen, hamburgerClick }) => {
     });
 
     return (
-        <>
-            <MenuLink
-                to="/redirect/instagram"
-                className="navbar-item is-hidden-desktop"
-            >
-                <span className="icon" style={{ color: '#333' }}>
-                    <FontAwesomeIcon icon={faInstagram} />
-                </span>
-            </MenuLink>
-            <div className={classes} onClick={hamburgerClick}>
-                <span />
-                <span />
-                <span />
-            </div>
-        </>
+        <div className={classes} onClick={hamburgerClick}>
+            <span />
+            <span />
+            <span />
+        </div>
     );
 };
 
