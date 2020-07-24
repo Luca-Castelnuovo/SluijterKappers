ea4aba2f6472ff5576faa4ee148137ee561145a9
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-07-24 13:09:00 +0200

    brand title responsive

--- src/Components/Hero/HeroBody.js
+++ src/Components/Hero/HeroBody.js
@@ -5,12 +5,21 @@ const HeroBody = () => {
     const HeroTitle = styled.h1`
         font-family: 'BankGothicBT-Medium', 'Didact Gothic', sans-serif;
         font-size: 6rem;
+
+        @media (max-width: 768px) {
+            font-size: 4rem;
+        }
+
+        @media (max-width: 465px) {
+            font-size: 2.8rem;
+        }
     `;
 
     return (
         <div className="hero-body has-text-centered">
             <HeroTitle className="title has-text-white">
-                Sluijter Kappers
+                Sluijter
+                <br /> Kappers
             </HeroTitle>
         </div>
     );
