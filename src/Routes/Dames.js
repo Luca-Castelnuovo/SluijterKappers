78e6c45dd639802434388a8275567624ffca47ed
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-07-24 21:54:59 +0200

    initital dames heren page

--- src/Routes/Dames.js
+++ src/Routes/Dames.js
@@ -1,11 +1,88 @@
 import React from 'react';
 import withBaseRoute from 'Utils/withBaseRoute';
+import Business2 from 'Assets/images/business/business2.jpg';
 
 const Dames = () => {
     return (
         <section className="section">
             <div className="container">
-                <h1 className="title">Dames</h1>
+                <div className="columns is-centered">
+                    <div className="column is-narrow is-size-4">
+                        <h1 className="title is-1">Dames</h1>
+
+                        <table className="table">
+                            <tbody>
+                                <tr>
+                                    <th>Wassen en föhnen</th>
+                                    <td>31,-</td>
+                                </tr>
+                                <tr>
+                                    <th>Wassen, knippen en drogen</th>
+                                    <td>53,-</td>
+                                </tr>
+                                <tr>
+                                    <th>Wassen, knippen en föhnen</th>
+                                    <td>59,-</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <h3 className="subtitle is-4 mb-0">Jeugd</h3>
+                        <table className="table">
+                            <tbody>
+                                <tr>
+                                    <th>Knippen (t/m 12 jaar)</th>
+                                    <td>27,-</td>
+                                </tr>
+                                <tr>
+                                    <th>Knippen (13 t/m 17)</th>
+                                    <td>38,-</td>
+                                </tr>
+                                <tr>
+                                    <th>Knippen (18 t/m 21)</th>
+                                    <td>40,-</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <h3 className="subtitle is-4 mb-0">Kleuren</h3>
+                        <table className="table">
+                            <tbody>
+                                <tr>
+                                    <th>Verven</th>
+                                    <td>vanaf 49,-</td>
+                                </tr>
+                                <tr>
+                                    <th>Highlights</th>
+                                    <td>vanaf 67,-</td>
+                                </tr>
+                                <tr>
+                                    <th>Folie kleuring (deel)</th>
+                                    <td>vanaf 80,-</td>
+                                </tr>
+                                <tr>
+                                    <th>Folie kleuring (compleet)</th>
+                                    <td>vanaf 145,-</td>
+                                </tr>
+                                <tr>
+                                    <th>French Balayage</th>
+                                    <td>vanaf 100,-</td>
+                                </tr>
+                                <tr>
+                                    <th>Permananet (compleet)</th>
+                                    <td>vanaf 120,-</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="column is-hidden-touch">
+                        <div className="column is-hidden-touch">
+                            <figure class="image is-5by4">
+                                <img src={Business2} alt="TODO" />
+                            </figure>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
--- src/Routes/Heren.js
+++ src/Routes/Heren.js
@@ -1,11 +1,62 @@
 import React from 'react';
 import withBaseRoute from 'Utils/withBaseRoute';
+import Business1 from 'Assets/images/business/business1.jpg';
 
 const Heren = () => {
     return (
         <section className="section">
             <div className="container">
-                <h1 className="title">Heren</h1>
+                <div className="columns is-centered">
+                    <div className="column is-narrow is-size-4">
+                        <h1 className="title is-1">Heren</h1>
+
+                        <table className="table">
+                            <tbody>
+                                <tr>
+                                    <th>Knippen</th>
+                                    <td>35,-</td>
+                                </tr>
+                                <tr>
+                                    <th>Wassen en knippen</th>
+                                    <td>39,-</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <h3 className="subtitle is-4 mb-0">Jeugd</h3>
+                        <table className="table">
+                            <tbody>
+                                <tr>
+                                    <th>Knippen (t/m 12 jaar)</th>
+                                    <td>27,-</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <h3 className="subtitle is-4 mb-0">Overig</h3>
+                        <table className="table">
+                            <tbody>
+                                <tr>
+                                    <th>Baard knippen</th>
+                                    <td>5,-</td>
+                                </tr>
+                                <tr>
+                                    <th>Tondeuse (5 min)</th>
+                                    <td>11,-</td>
+                                </tr>
+                                <tr>
+                                    <th>Tondeuse (uitgebreid)</th>
+                                    <td>22,-</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="column is-hidden-touch">
+                        <figure class="image is-5by4">
+                            <img src={Business1} alt="TODO" />
+                        </figure>
+                    </div>
+                </div>
             </div>
         </section>
     );
