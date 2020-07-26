defdde0dc88f87576894e8da7ce24cb2d1624934
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-07-26 15:38:43 +0200

    emphesise current day more

--- src/Components/Footer/FooterHours.js
+++ src/Components/Footer/FooterHours.js
@@ -13,25 +13,25 @@ const FooterHours = () => {
             <p className="title">Openingstijden</p>
 
             <List className="has-text-left is-size-4">
-                <li className={day === 1 ? 'has-text-weight-semibold' : ''}>
+                <li className={day === 1 ? 'has-text-weight-bold' : ''}>
                     Maandag - gesloten
                 </li>
-                <li className={day === 2 ? 'has-text-weight-semibold' : ''}>
+                <li className={day === 2 ? 'has-text-weight-bold' : ''}>
                     Dinsdag - 9.00 - 17.30
                 </li>
-                <li className={day === 3 ? 'has-text-weight-semibold' : ''}>
+                <li className={day === 3 ? 'has-text-weight-bold' : ''}>
                     Woensdag - 9.00 - 17.30
                 </li>
-                <li className={day === 4 ? 'has-text-weight-semibold' : ''}>
+                <li className={day === 4 ? 'has-text-weight-bold' : ''}>
                     Donderdag - 9.00 - 17.30
                 </li>
-                <li className={day === 5 ? 'has-text-weight-semibold' : ''}>
+                <li className={day === 5 ? 'has-text-weight-bold' : ''}>
                     Vrijdag - 9.00 - 17.30
                 </li>
-                <li className={day === 6 ? 'has-text-weight-semibold' : ''}>
+                <li className={day === 6 ? 'has-text-weight-bold' : ''}>
                     Zaterdag - 9.00 - 16.00
                 </li>
-                <li className={day === 0 ? 'has-text-weight-semibold' : ''}>
+                <li className={day === 0 ? 'has-text-weight-bold' : ''}>
                     Zondag - gesloten
                 </li>
             </List>
