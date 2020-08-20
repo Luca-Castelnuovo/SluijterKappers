1da38db6d68274f845ea1d84c7d0fb6ae1f98253
PepijnRezelman <pepijnrezelman@gmail.com>
2020-08-20 17:23:22 +0200

    Added Teams page

--- src/Routes/Team.js
+++ src/Routes/Team.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import withBaseRoute from 'Utils/withBaseRoute';
+import TeamMembers from 'Components/Team/TeamMembers';
 
 const Team = () => {
-    return (
-        <section className="section">
-            <div className="container">
-                <h1 className="title is-size-1">Team</h1>
-            </div>
-        </section>
-    );
+    return <TeamMembers />;
 };
 
 export default withBaseRoute(Team);
