e74a8e3129e50cf835f22d678f51803156666608
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-09-02 12:44:51 +0200

    upload new logo's

--- src/Components/Footer/FooterBrands.js
+++ src/Components/Footer/FooterBrands.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import doubleTrue from 'Assets/images/brands/double_true.jpg';
 import tecniArt from 'Assets/images/brands/tecni_art.jpg';
 import kerastase from 'Assets/images/brands/kerastase.jpg';
-import loreal from 'Assets/images/brands/loreal.jpg';
+import lorealProfessionel from 'Assets/images/brands/loreal_professionel.jpg';
 import marcInbane from 'Assets/images/brands/marc_inbane.jpg';
 
 const Logo = styled.figure`
@@ -46,8 +46,8 @@ const FooterBrands = () => {
                     <div className="column is-narrow">
                         <Logo className="image">
                             <img
-                                alt="L'Oréal || Sluijter Kappers"
-                                src={loreal}
+                                alt="L'Oréal Professionel || Sluijter Kappers"
+                                src={lorealProfessionel}
                             />
                         </Logo>
                     </div>
