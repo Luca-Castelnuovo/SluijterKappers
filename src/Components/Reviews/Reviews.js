0f26e5182d263452976b17ba9ae452edafb64199
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-09-06 20:58:16 +0200

    import wrong file

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,13 +1,25 @@
 import React from 'react';
-// import ReviewsItem from './ReviewsItem';
-import useReviews from 'Utils/useProducts';
+import ReviewsItem from './ReviewsItem';
+import useReviews from 'Utils/useReviews';
 
 const Reviews = () => {
     const ReviewsData = useReviews();
 
-    console.log(ReviewsData);
+    if (!ReviewsData) {
+        return (
+            <section className="section">
+                <div className="container">
+                    <h2 className="title has-text-centered is-size-1 has-text-weight-medium">
+                        Reviews
+                    </h2>
+                    <div className="tile is-ancestor">
+                        <progress class="progress is-large is-primary"></progress>
+                    </div>
+                </div>
+            </section>
+        );
+    }
 
-    // if (!ReviewsData) {
     return (
         <section className="section">
             <div className="container">
@@ -15,27 +27,13 @@ const Reviews = () => {
                     Reviews
                 </h2>
                 <div className="tile is-ancestor">
-                    <progress class="progress is-large is-primary"></progress>
+                    {ReviewsData.map((review) => (
+                        <ReviewsItem key={review.name} review={review} />
+                    ))}
                 </div>
             </div>
         </section>
     );
-    // }
-
-    // return (
-    //     <section className="section">
-    //         <div className="container">
-    //             <h2 className="title has-text-centered is-size-1 has-text-weight-medium">
-    //                 Reviews
-    //             </h2>
-    //             <div className="tile is-ancestor">
-    //                 {ReviewsData.map((review) => (
-    //                     <ReviewsItem key={review.name} review={review} />
-    //                 ))}
-    //             </div>
-    //         </div>
-    //     </section>
-    // );
 };
 
 export default Reviews;
