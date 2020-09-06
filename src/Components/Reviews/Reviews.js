d46b9da56504c342442936010f01063d5e95b8bf
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-09-06 21:04:42 +0200

    clean up code

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,25 +1,7 @@
 import React from 'react';
-import ReviewsItem from './ReviewsItem';
-import useReviews from 'Utils/useReviews';
+import ReviewsContainer from './ReviewsContainer';
 
 const Reviews = () => {
-    const ReviewsData = useReviews();
-
-    if (!ReviewsData) {
-        return (
-            <section className="section">
-                <div className="container">
-                    <h2 className="title has-text-centered is-size-1 has-text-weight-medium">
-                        Reviews
-                    </h2>
-                    <div className="tile is-ancestor">
-                        <progress class="progress is-large is-primary"></progress>
-                    </div>
-                </div>
-            </section>
-        );
-    }
-
     return (
         <section className="section">
             <div className="container">
@@ -27,9 +9,7 @@ const Reviews = () => {
                     Reviews
                 </h2>
                 <div className="tile is-ancestor">
-                    {ReviewsData.map((review) => (
-                        <ReviewsItem key={review.name} review={review} />
-                    ))}
+                    <ReviewsContainer />
                 </div>
             </div>
         </section>
---
+++ src/Components/Reviews/ReviewsContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReviewsItem from './ReviewsItem';
+import useReviews from 'Utils/useReviews';
+
+const ReviewsContainer = () => {
+    const ReviewsData = useReviews();
+
+    if (!ReviewsData) {
+        return <progress class="progress is-large is-primary"></progress>;
+    }
+
+    return ReviewsData.map((review) => (
+        <ReviewsItem key={review.name} review={review} />
+    ));
+};
+
+export default ReviewsContainer;
