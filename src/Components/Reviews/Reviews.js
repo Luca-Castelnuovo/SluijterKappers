b7fab7633f54eeb95f44e186789be74dc8cb0c81
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-09-06 20:54:26 +0200

    tmp debug

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import ReviewsItem from './ReviewsItem';
+// import ReviewsItem from './ReviewsItem';
 import useReviews from 'Utils/useProducts';
 
 const Reviews = () => {
     const ReviewsData = useReviews();
 
-    if (!ReviewsData) {
-        return (
-            <section className="section">
-                <div className="container">
-                    <h2 className="title has-text-centered is-size-1 has-text-weight-medium">
-                        Reviews
-                    </h2>
-                    <div className="tile is-ancestor">
-                        <progress class="progress is-large is-primary"></progress>
-                    </div>
-                </div>
-            </section>
-        );
-    }
+    console.log(ReviewsData);
 
+    // if (!ReviewsData) {
     return (
         <section className="section">
             <div className="container">
@@ -27,13 +15,27 @@ const Reviews = () => {
                     Reviews
                 </h2>
                 <div className="tile is-ancestor">
-                    {ReviewsData.map((review) => (
-                        <ReviewsItem key={review.name} review={review} />
-                    ))}
+                    <progress class="progress is-large is-primary"></progress>
                 </div>
             </div>
         </section>
     );
+    // }
+
+    // return (
+    //     <section className="section">
+    //         <div className="container">
+    //             <h2 className="title has-text-centered is-size-1 has-text-weight-medium">
+    //                 Reviews
+    //             </h2>
+    //             <div className="tile is-ancestor">
+    //                 {ReviewsData.map((review) => (
+    //                     <ReviewsItem key={review.name} review={review} />
+    //                 ))}
+    //             </div>
+    //         </div>
+    //     </section>
+    // );
 };
 
 export default Reviews;
