dfc852de6c0fc0de1b4c3b0d361d60e1ae27441f
Luca Castelnuovo <luca@castelnuovo.xyz>
2022-09-01 19:45:04 +0200

    Update Dames.js

--- src/Config/Dames.js
+++ src/Config/Dames.js
@@ -1,24 +1,24 @@
 const Dames = [
     {
         title: '',
-        'Wassen en föhnen': 32
-        'Wassen, knippen en drogen': 59
+        'Wassen en föhnen': 32,
+        'Wassen, knippen en drogen': 59,
         'Wassen, knippen en föhnen': 64
     },
     {
         title: 'Jeugd',
-        'Knippen (t/m 12 jaar)': 29
-        'Knippen (13 t/m 18 jaar)': 38
+        'Knippen (t/m 12 jaar)': 29,
+        'Knippen (13 t/m 18 jaar)': 38,
         'Knippen (19 t/m 23 jaar)': 42
     },
     {
         title: 'Kleuren',
-        Verven: 'vanaf 51,50
-        ,Highlights: 'vanaf 74,-
-        'Folie kleuring (deel)': 'vanaf 89,-
-        'Folie kleuring (compleet)': 'vanaf 150,-
-        'French Balayage': 'vanaf 150’
-        'Permanent (compleet)': 'vanaf 135
+        Verven: 'vanaf 51,50,
+        ,Highlights: 'vanaf 74',
+        'Folie kleuring (deel)': 'vanaf 89',
+        'Folie kleuring (compleet)': 'vanaf 150',
+        'French Balayage': 'vanaf 150',
+        'Permanent (compleet)': 'vanaf 135'
     },
 ];
 
